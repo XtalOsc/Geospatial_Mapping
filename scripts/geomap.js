995bf998b012a65a483b67c246603f5e717ef648
Xtal Osc <xtalosc@gmx.com>
2017-01-22 15:40:56 -0600

    us map

--- scripts/geomap.js
+++ scripts/geomap.js
@@ -1,20 +1,34 @@
 console.log("in geomap.js")
 
-var width = 960;
-var height = 600;
+var width = 960,
+    height = 600;
 
 var path = d3.geo.path()
-.projection(null);
+    .projection(null);
 
 var svg = d3.select("body").append("svg")
-.attr("width", width)
-.attr("height", height);
+    .attr("width", width)
+    .attr("height", height);
 
 d3.json("./data/us.json", function(error, us) {
-  if (error) return console.error(error);
+  if (error) throw error;
 
+	//draw base
   svg.append("path")
-  .datum(topojson.feature(us, us.objects.states))
-  .attr("class", "land")
-  .attr("d", path);
+      .datum(topojson.feature(us, us.objects.nation))
+      .attr("class", "land")
+      .attr("d", path);
+
+	//add state borders
+	svg.append("path")
+			.datum(topojson.mesh(us, us.objects.states))
+			.attr("class", "border state")
+			.attr("d", path);
+
+	//add county borders
+  svg.append("path")
+      .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; }))
+      .attr("class", "border county")
+      .attr("d", path);
+
 });
